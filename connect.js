534031875cf205ed6a78c775f2470bb3dbde01f7
VY <156393258+Gsjksgwgww@users.noreply.github.com>
2025-04-30 00:29:39 +0300

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,9 +1,1 @@
-const modal = new window.Web3Modal({
-  projectId: '8f3eb896d127f6a619e7d78b1085647f', // твой Project ID
-  walletConnectVersion: 2,
-  themeMode: 'dark'
-});
 
-document.getElementById('connectBtn').addEventListener('click', () => {
-  modal.openModal();
-});
